38c77f367a456852c991f7622e03ed54d68946f7
Agustin <agustin.bringas.2107@gmail.com>
2022-01-06 23:37:43 -0300

    Task 1 functionally finished. Gotta improve CSS

--- src/MovieLibrary/components/MoviesList.js
+++ src/MovieLibrary/components/MoviesList.js
@@ -1,23 +1,27 @@
 import React, { useState } from 'react'
 import TMDBImage from './TMDBImage'
 import './MoviesList.css'
+import { ReactComponent as Star } from './star.svg';
+
 
 export default function MoviesList ({ movies }){
 
 
   const [selectedMovie, setSelectedMovie] = useState(null)
   const [sortingType, setSortingType] = useState('')
-  const handleSelectMovie = movie => setSelectedMovie(movie)
   const handleSortingChange = event => {
     setSortingType(event.target.value)
   }
+  const handleSelectMovie = movie => setSelectedMovie(movie)
+  const onClose = () => setSelectedMovie(null)
+  
 
   return(<div className="movies-list">
-    <div className="items">
       <div>
         <span>Sort by:</span>
         <SortingOptions selectedOption={sortingType} onChange={handleSortingChange}/>
       </div>
+    <div className="items">
       {
         movies.map(movie =>
           <MovieListItem key={movie.id} movie={movie} isSelected={selectedMovie===movie} onSelect={handleSelectMovie}/>
@@ -26,7 +30,7 @@ export default function MoviesList ({ movies }){
     </div>
     {
       selectedMovie && (
-        <ExpandedMovieItem movie={selectedMovie} />
+        <ExpandedMovieItem movie={selectedMovie} onClose={onClose}/>
       )
     }
   </div>)
@@ -34,22 +38,30 @@ export default function MoviesList ({ movies }){
   
 }
 
-const ExpandedMovieItem = ({movie: {title, original_title, poster_path, overview, vote_average, vote_count}}) => (
-  <div className="expanded-movie-item">
-    <TMDBImage src={poster_path} className="poster" />
-    <div className="description">
-      <h2>{title}({original_title})</h2>
-      <div><h4>Rank(votes count)</h4>: <span>{vote_average}({vote_count})</span></div>
-      <span>{overview}</span>
+const ExpandedMovieItem = ({movie: {title, original_title, backdrop_path, overview, vote_average, vote_count}, onClose}) => (
+  <div className="movie-modal-container" id="movie-modal-container">
+    <div className="expanded-movie-modal">
+      <TMDBImage src={backdrop_path} className="poster" />
+      <div className="description">
+        <h2>{title}({original_title})</h2>
+        <div><h4>Rank(votes count)</h4>: <span>{vote_average}({vote_count})</span></div>
+        <span>{overview}</span>
+        <button onClick={onClose}>Close</button>
+      </div>
     </div>
   </div>
 )
 
 function MovieListItem ({movie, isSelected, onSelect}) {
   const handleClick = () => onSelect(movie)
-  const { title, vote_average } = movie
-  const className = `movie-list-item ${isSelected ? 'selected' : ''}`
-  return(<div className={className} onClick={handleClick}>{title}({vote_average})</div>)
+  return(
+    <div className="movie-card-container">
+      <div className="movie-card" onClick={handleClick}>
+          <TMDBImage src={movie.poster_path} className="movie-card-img"/>
+          <span className="movie-title">{movie.title}</span>
+          <span className="movie-votes"><Star className="star-symbol"/>{movie.vote_average}</span>
+      </div>
+    </div>)
 }
 
 function SortingOptions ({ selectedOption, onChange }) {
